6b0c92c1d9aae888d56fd2cfd4a6f78801016e8c
Xueqiong Zhang <joan.zhang@emory.edu>
2025-10-10 16:58:33 -0400

    Document IAM_ROLE usage in source loading script

--- PSYCHIATRY/Builder/sql/3a_load-source-prep.sql
+++ PSYCHIATRY/Builder/sql/3a_load-source-prep.sql
@@ -13,5 +13,9 @@ TRUNCATE TABLE public.psych_mapping_emory;
 -- Load data from S3 Parquet file
 COPY public.psych_mapping_emory 
 FROM 's3://winship-cars/joan/CustomConcepts_Emory.parquet'
+-- Using IAM credentials (temporary keys injected by script)
 CREDENTIALS 'aws_access_key_id={ACCESS_KEY};aws_secret_access_key={SECRET_KEY};token={SESSION_TOKEN}'
+
+-- Alternatively, using IAM_ROLE is easier and more secure, but you need to ask your DBA to set it up:
+-- IAM_ROLE 'arn:aws:iam::123456789012:role/YourRedshiftRole'
 FORMAT AS PARQUET;
